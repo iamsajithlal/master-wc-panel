9428712fd484a64e25784c940458ca6385267bc0
Sajithlal <iamsajithlal@gmail.com>
2022-02-17 00:09:07 +0530

    style changes

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Grid } from "@mui/material";
 import useStyles from "./style";
 
-const Login = () =>{
+const Login = () => {
     const classes = useStyles()
-    return(
-       <div className={classes.root}>Helloo</div>
+    return (
+        <Grid container>
+            <Grid container
+                item xs={4}
+                direction="column">
+                <div className={classes.leftPanel}>
+
+                </div>
+            </Grid>
+            <Grid container
+                item xs={8}
+                direction="column" >
+
+            </Grid>
+        </Grid>
     )
 }
 
